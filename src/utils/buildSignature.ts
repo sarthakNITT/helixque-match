8639b0bbe00419d8fc217a3d8dbe1b78eff666b4
sarthakNITT <sarthakkarodework@gmail.com>
2025-12-12 01:58:05 +0530

    feat: added signature logic phase-1

--- src/utils/buildSignature.ts
+++ src/utils/buildSignature.ts
@@ -4,22 +4,30 @@
 // experience: "1-3",
 // country: "IN",
 
-export const BuildSignature = async (props: any) => {
-    const language = Check(props.language);
-    const domain = Check(props.domain);
-    const techStack = Check(props.techStack);
-    const experience = Check(props.experience);
-    const country = Check(props.country);
-    const key = `language:${language}|domain:${domain}|techStack:${techStack}|experience:${experience}|country:${country}`;
-    return key;
+export interface UserPreferences {
+  techStack: string | string[];
+  language: string | string[];
+  experience: string | string[];
+  domain: string | string[];
+  country: string | string[];
 }
 
-function Check (v: string) {
-    if(Array.isArray(v)){
-        return v
-        .map((x) => String(x).toLowerCase())
-        .sort((a,b) => a.localeCompare(b))
-        .join(",")
-    }
-    return String(v).toLowerCase();
+export const BuildSignature = async (props: UserPreferences) => {
+  const language = Check(props.language);
+  const domain = Check(props.domain);
+  const techStack = Check(props.techStack);
+  const experience = Check(props.experience);
+  const country = Check(props.country);
+  const key = `language:${language}|domain:${domain}|techStack:${techStack}|experience:${experience}|country:${country}`;
+  return key;
+};
+
+function Check(v: string | string[]) {
+  if (Array.isArray(v)) {
+    return v
+      .map((x) => String(x).toLowerCase())
+      .sort((a, b) => a.localeCompare(b))
+      .join(",");
+  }
+  return String(v).toLowerCase();
 }
