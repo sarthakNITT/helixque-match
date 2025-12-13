5cdadc837b474ec74e98aef52947750973821b72
sarthakNITT <sarthakkarodework@gmail.com>
2025-12-13 14:15:27 +0530

    fix: minor change in app

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import corsPlugin from "./plugins/cors";
 import swaggerPlugin from "./plugins/swagger";
 import routes from "./routes";
 import healthRoute from "./routes/health";
-import logger from "./utils/logger";
+import { log as logger } from "./utils/logger";
 
 export const buildApp = (): FastifyInstance => {
-  const app = Fastify({ logger });
+  const app = Fastify({ logger: true });
 
   app.register(corsPlugin);
   app.register(swaggerPlugin);
